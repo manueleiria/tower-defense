812a2d8a271d8254ec8cc50208955e757ee267f3
Joana Contente <joanacontente@gmail.com>
2016-05-27 16:33:46 +0100

    added SimpleGfxPlayerBase and added PlayerBase properties

--- src/org/academiadecodigo/towerdefense/object/gameobject/PlayerBase.java
+++ src/org/academiadecodigo/towerdefense/object/gameobject/PlayerBase.java
@@ -1,15 +1,75 @@
 package org.academiadecodigo.towerdefense.object.gameobject;
 
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseEventType;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
 import org.academiadecodigo.towerdefense.object.representable.Representable;
 
 /**
  * Created by codecadet on 23/05/16.
  */
-public class PlayerBase extends AbstractObject {
+public class PlayerBase extends AbstractObject implements MouseHandler{
+
+    private int hitPoints = 100;
+    private Mouse mouse;
+    private int mouseX;
+    private int mouseY;
+
+
+
+    public int getMouseX() {
+        return mouseX;
+    }
+
+    public int getMouseY() {
+        return mouseY;
+    }
+
+
 
 
     public PlayerBase(Representable representation) {
 
         super(representation, GameObjectType.PLAYER_BASE);
+        mouse = new Mouse(this);
+
     }
+
+
+
+
+
+
+    public int decreaseHitPoints(int damage) {
+        return hitPoints -= damage;
+    }
+
+
+    // TODO: implement shoot interface
+
+
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+
+
+
+        mouse.addEventListener(MouseEventType.MOUSE_CLICKED);
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+
+        //confirmar posição
+        mouse.addEventListener(MouseEventType.MOUSE_MOVED);
+
+    }
+
+
 }
--- src/org/academiadecodigo/towerdefense/object/simplegfx/SimpleGfxPlayerBase.java
+++ src/org/academiadecodigo/towerdefense/object/simplegfx/SimpleGfxPlayerBase.java
@@ -1,31 +1,21 @@
 package org.academiadecodigo.towerdefense.object.simplegfx;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.towerdefense.object.gameobject.GameObjectType;
 import org.academiadecodigo.towerdefense.object.gameobject.PlayerBase;
 import org.academiadecodigo.towerdefense.object.representable.Representable;
 
 /**
  * Created by codecadet on 25/05/16.
  */
-public class SimpleGfxPlayerBase extends PlayerBase {
+public class SimpleGfxPlayerBase extends SimpleGfxRepresentation {
 
-    private int hitPoints = 100;
-    private static final int WIDTH = 100;
-    private static final int HEIGHT = 100;
     private static final double MARGIN_LEFT = 10;
     private static final double MARGIN_TOP = 10;
 
 
 
-    public int getHitPoints() {
-        return hitPoints;
-    }
-
-    public void setHitPoints(int damage) {
-        this.hitPoints -= damage;
-    }
-
-    public SimpleGfxPlayerBase() {
-        super((Representable) new Rectangle(MARGIN_LEFT, MARGIN_TOP, WIDTH, HEIGHT));
+    public SimpleGfxPlayerBase(GameObjectType type) {
+        super(new Rectangle(MARGIN_LEFT, MARGIN_TOP, type.getCols() * getCellSize(), type.getRows() * getCellSize()));
     }
 }
