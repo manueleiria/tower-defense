0c8e87924fda3d0761c86374ae13d1e2db5ace96
Manuel Leiria <manueleiria@gmail.com>
2016-05-30 18:59:17 +0100

    minor addition

--- src/org/academiadecodigo/towerdefense/object/gameobject/AbstractObject.java
+++ src/org/academiadecodigo/towerdefense/object/gameobject/AbstractObject.java
@@ -1,23 +1,34 @@
 package org.academiadecodigo.towerdefense.object.gameobject;
 
+import org.academiadecodigo.simplegraphics.mouse.Mouse;
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
 import org.academiadecodigo.towerdefense.object.interfaces.Representable;
 
 /**
  * Created by codecadet on 23/05/16.
  */
-public abstract class AbstractObject {
+public abstract class AbstractObject implements MouseHandler {
     private Representable representation;
     private GameObjectType type;
     private int xPos;
     private int yPos;
+    private Mouse mouse;
+
 
 
     public AbstractObject(Representable representation, GameObjectType type, int xPos, int yPos) {
 
+        mouse = new Mouse(this);
         this.representation = representation;
         this.type = type;
         this.xPos = xPos;
         this.yPos = yPos;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
     }
 
     public void movePos(int dx, int dy) {
@@ -34,7 +45,6 @@ public abstract class AbstractObject {
     }
 
     public Representable getRepresentation() {
-
         return representation;
     }
 }
